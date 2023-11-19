cf72191af5c9b265a00be14c7d6fcfdf5144bd86
VuvuM03 <139034985+VuvuM03@users.noreply.github.com>
2023-11-19 14:31:31 +0200

    Updated loader to blue

--- src/Weather.js
+++ src/Weather.js
@@ -24,7 +24,7 @@ export default function Weather(props) {
         height="80"
         width="80"
         radius="9"
-        color="green"
+        color="blue"
         ariaLabel="three-dots-loading"
         wrapperStyle
         wrapperClass
