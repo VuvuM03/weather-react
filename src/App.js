7b2cf015d4b60ca3b36f659247239713674a8355
VuvuM03 <139034985+VuvuM03@users.noreply.github.com>
2023-11-20 21:45:29 +0200

    Updated Github and Netlify links

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@ function App() {
       <h1>Weather App</h1>
       <Weather />
       <footer>
-        Coded by
-        <a href="https://app.netlify.com/sites/courageous-daffodil-72d1bf/configuration/general">
-          VuvuM
+        <a href="https://github.com/VuvuM03/weather-react">Open-source code</a>,
+        by Vuvu M, hosted on
+        <a
+          href="https://courageous-daffodil-72d1bf.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
         </a>
       </footer>
     </div>
