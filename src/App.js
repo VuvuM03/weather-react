d52ed3b735816d917359e2dcb8040c4707edf666
VuvuM03 <139034985+VuvuM03@users.noreply.github.com>
2023-11-20 19:42:22 +0200

    Transferred Project from CodeSandbox to VSCode

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World !!!!</h1>
-        <Weather city="Pretoria" />
-      </header>
+      <h1>Weather App</h1>
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,35 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Hourglass } from "react-loader-spinner";
 
-export default function Weather(props) {
+export default function Weather() {
+  const [city, setCity] = useState("durban");
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+
   function showWeather(response) {
-    alert(
-      `The weather in ${response.data.name} is ${Math.round(
-        response.data.main.temp
-      )}°C `
-    );
+    setLoading(true);
+    setWeather({
+      description: response.data.condition.description,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.condition.description,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+    });
   }
 
-  let apiKey = "ff1d9ea9376b5c27a82e04fc2b2abdbb";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "69fo350cf347a61tc6a94bf3497a464e";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
 
-  //api.openweathermap.org/data/2.5/weather?q=$[props.city]&appid=[apiKey]&units=metric
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
-  axios.get(apiUrl).then(showWeather);
-
-  return (
-    <div>
-      <Hourglass
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="hourglass-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        colors={["#306cce", "#72a1ed"]}
-      />
-      <h2>Hello Weather</h2>;
-    </div>
-  );
+  if (loading) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            className="search-input"
+            placeholder="Enter a city....."
+            onChange={updateCity}
+          />
+          <br />
+          <input type="submit" className="submit-btn" />
+        </form>
+        <div>
+          <ul>
+            <li>Description: {weather.description}</li>
+            <li>Temperature: {Math.round(weather.temperature)}℃</li>
+            <li>Humidity: {weather.humidity}%</li>
+            <li>Wind Speed: {weather.wind}km/h</li>
+          </ul>
+          <img src={weather.icon} alt="weather_icon" />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Enter a city....."
+          onChange={updateCity}
+        />
+        <br /> <br />
+        <input type="submit" className="submit-btn" />
+      </form>
+    );
+  }
 }
